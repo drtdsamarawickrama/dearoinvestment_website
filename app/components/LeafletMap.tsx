a2356440e13c1a6a9afd77761f695bb1460d3d89
drtdsamarawickrama <drtdsamarawickrama@gmail.com>
2025-12-22 14:06:03 +0530

    VC error fix

---
+++ app/components/LeafletMap.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fix default marker icon issue
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+export default function LeafletMap() {
+  return (
+    <MapContainer
+      center={[6.9271, 79.8612]} // Colombo
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[6.9271, 79.8612]}>
+        <Popup>Dearo Investment</Popup>
+      </Marker>
+    </MapContainer>
+  );
+}
