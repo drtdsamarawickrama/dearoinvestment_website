1f2b16e2a772786b519642661599d26ea573011d
Denuwan <sandeepadenuwan980@gmail.com>
2026-01-05 09:22:53 +0530

    update img

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -89,12 +89,12 @@ export default function BoardMembersPage() {
      {
       name: "Mr. T. Sinthushan",
       title: "Branch Manager – Mannar",
-      img: "/assests/management/iresh.jpeg",
+      img: "/assests/management/manna.jpg",
     }, 
     {
       name: "Mr. J.J.Moraies",
       title: "Branch Manager – Kaluwanchikudy",
-      img: "/assests/management/iresh.jpeg",
+      img: "/assests/management/kaluwan.jpg",
     },
   ];
 
